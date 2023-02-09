c33cb49ce45e787e199f07a6b245b14102445668
Темирлан Абдраманов <froskyta.dev@gmail.com>
2023-02-10 03:08:24 +0600

    refactor for github-pages

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model, Types } = require('mongoose');
-
-const schema = Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    links: [{type: Types.ObjectId }]
-});
-
-module.exports = model('User', schema);
